047e563acc6fbc95cc780738dec16821cb7e869a
Alinaa12345 <61905308+Alinaa12345@users.noreply.github.com>
2020-08-14 18:09:52 +0200

    Add files via upload

---
+++ LoginValid.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+public class LoginValid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.opera.driver", "E:\\pobrane\\operadriver.exe");
+		WebDriver driver = new OperaDriver();
+		driver.manage().window().maximize();
+		driver.get("https://orangehrm-demo-6x.orangehrmlive.com/");
+		driver.findElement(By.id("txtUsername")).clear();
+		driver.findElement(By.id("txtUsername")).sendKeys("admin");
+		driver.findElement(By.name("txtPassword")).clear();
+		driver.findElement(By.name("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.cssSelector("input#btnLogin")).click();
+	}
+
+}
